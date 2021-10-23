92bb1e2a9b538037ed84ae0c48fcc8bdfebba7c7
sinker048 <sinker048@gmail.com>
2021-10-23 15:29:27 +0800

    practicing sting

---
+++ get_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string str;
+    cout<<"Please enter your name"<<endl;
+    getline(cin, str);
+    cout<<str<<endl;
+    return 0;
+}
